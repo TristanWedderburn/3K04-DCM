48d04a943d742bfd0fdb77c2cc9b499260d197a7
Khang Nguyen <nnmkhang166@gmail.com>
2018-10-18 18:11:36 -0400

    Added Comments for clarity

--- DCM-GUI.py
+++ DCM-GUI.py
@@ -6,19 +6,19 @@
 
 # usernames will be stored in a hashtable and then checked. 
 
-ref = {} # initally empty
+ref = {} # creates an empty dictonary (also know as hashtable)
  
 flag = True
 def newUser():
-    if len(ref) >= 3:
-        print("Error only 10 users allowed")
-        flag = False
+    if len(ref) >= 3: # 3 only used so you donth have to type 10 
+        print("Error only 10 users allowed")  
+        x = input("press any key to continue") # temporary buffer
         
     else:
         user = input("Please enter a new username") # gets the users name for the hashtable
-        password = input("please enter a password")
-        ref[user] = password
-        x = input("Press any key to continue")
+        password = input("please enter a password") # propmpts user for input.
+        ref[user] = password # adds new key--value pair into the dictonary 
+        x = input("Press any key to continue") # buffer to stop  this is temporary 
 
 
 def login():
@@ -26,8 +26,8 @@ def login():
 
 
 while flag:
-    newUser()
-    print(ref)
+    newUser() # calling newUser function
+    print(ref) # prints out the new ref 
 
 
 
