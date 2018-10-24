f649433b25e167e2bfeaa0823b396a517ab1b360
sidaksran <sidak_sran@hotmail.com>
2018-10-23 21:24:24 -0400

    GUI
    updated GUI

--- sid_test.py
+++ sid_test.py
@@ -3,83 +3,35 @@
 # 3k04 milestone1
 
 from tkinter import *
-import os
-
-# creds = 'tempfile.temp'
+from tkinter import ttk
 
 # GUI
 
-def Signup():
-    global root
-    global passwordE
-    global usernameE
+gui = Tk()
+gui.geometry("400x400")
 
-    root = Tk()
-    root.title('LABron Pacemaker')
-    instruction = Label(root, text="Please sign up.")
-    instruction.grid(row=0, column=0, sticky=E)
+def login(us1,pw1,usr,pword):
+    if us1 == usr and pw1 == pword:
+        i=Label(gui,text='Login success').grid(row=6,column=0)
+        print("Login success")
+    else:
+        j=Label(gui,text='Login failed').grid(row=6,column=0)
+        print("Wrong password")
 
-    username = Label(root, text="New Username: ")
-    password = Label(root, text="New Password: ")
-    username.grid(row=1, column=0, sticky=W)
-    password.grid(row=2, column=0, sticky=W)
+gui.title("LABron Pacemaker")
 
-    usernameE = Entry(root)
-    passwordE = Entry(roots, show='*')
-    
-    signupButton = Button(root, text='Signup', command=FSSignup)
-    signupButton.grid(columnspan=2, sticky=W)
+# Create Account Section
+uname = Label(gui ,text="username").grid(row=0,column=0)
+pword = Label(gui ,text="password").grid(row=1,column=0)
+u_entry = Entry(gui).grid(row=0,column=1)
+p_entry = Entry(gui,show="*").grid(row=1,column=1)
+createButton = Button(gui,text="Create account").grid(row=2,column=0)
 
-    root.mainloop() # makes sure window doesn't close quickly
+# Login Section
+uname1 = Label(gui ,text="username").grid(row=3,column=0)
+pword1 = Label(gui ,text="password").grid(row=4,column=0)
+u_entry1 = Entry(gui).grid(row=3,column=1)
+p_entry1 = Entry(gui,show="*").grid(row=4,column=1)
+loginButton = Button(gui,text="Login",command=lambda : login(u_entry,p_entry,u_entry1,p_entry1)).grid(row=5,column=0)
 
-def FSSignup():
-    with open(creds, 'w') as f:
-        f.write(usernameE.get())
-        f.write('\n')
-        f.write(passwordE.get())
-        f.close()
-    
-    root.destroy()
-    Login()
-
-def Login():
-    global Name
-    global Pass
-
-    root1 = Tk()
-    root1.title('Login')
-
-    instruction = Label(root1, text='Please Login\n')
-    instruction.grid(sticky=E)
-
-    username = Label(root1, text='Username: ')
-    password = Label(root1, text='Password: ')
-    username.grid(row=1, sticky=W)
-    password.grid(row=2, sticky=W)
-
-    usernameEL = Entry(root1)
-    passwordEL = Entry(root1, show='*')
-    usernameEL.grid(row=1, column=1)
-    passwordEL.grid(row=2, column=1)
-
-    loginButton = Button(root1, text='Login', command=CheckLogin)
-    loginButton.grid(columnspan=2, sticky=W)
-
-    root1.mainloop()
-
-def CheckLogin():
-    with open(creds) as f:
-        date = f.readlines()
-        uname = data[0].rstrip()
-        pword = data[1].rstrip()
-    
-    if Name.get() == uname and Pass == pword:
-        r = Tk()
-        r.title('Logged In')
-        r.geometry('150x50')
-        r.mainloop()
-    
-if os.path.isfile(creds):
-    Login()
-else:
-    Signup()
+gui.mainloop() # displays GUI
