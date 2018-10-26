b1cf91a9be1afbf9fd9ca15610a3d687c1c952e8
Tristan Wedderburn <tristan.wedderburn8@gmail.com>
2018-10-26 01:32:21 -0400

    debug

--- tristan_test/main.py
+++ tristan_test/main.py
@@ -31,27 +31,30 @@ from registerUser import  *
 
 # main_screen()
 
-class main_screen:
-    def __init__(self, master):
-        self.master = master
-        master.title("PACEMAKER 1.0")
+class StartPage(Frame):
+	def __init__(self, parent, controller):
+		Frame.__init__(self, parent)
+
+        parent.title("PACEMAKER 1.0")
 
         label = Label(master, text="Welcome to the PACEMAKER 1.0")
         label.grid(row=0, column=0)
 
-        self.login_button = Button(master, text="Login", command=login(screen))
+        self.login_button = Button(master, text="Login")
         self.login_button.grid(row=1, column=0)
 
-        self.register_button = Button(master, text="Register", command=register)
+        self.register_button = Button(master, text="Register")
         self.register_button.grid(row=2, column=0)
 
         self.exit_button = Button(master, text="Exit", command=master.quit)
         self.exit_button.grid(row=3, column=0)
 
         # if checkComm() == True:
-            Label(text = "Device is IS communicating with the DCM",fg = "green").grid(row=4)
+        Label(text = "Device is IS communicating with the DCM").grid(row=4, column=0)
+        Label(text ="o",fg = "green").grid(row=4, column=1)
         # elif checkComm() == False:
-            Label(text = "Device is NOT communicating with the DCM",fg ="red").grid(row=5)
+        Label(text = "Device is NOT communicating with the DCM").grid(row=5, column=0)
+        Label(text ="o",fg = "red").grid(row=5, column=1)
 
         master.columnconfigure(0, weight=1)
         master.rowconfigure(0, weight=1)
@@ -59,7 +62,37 @@ class main_screen:
         self.register_button.columnconfigure(0, weight=1)
         self.exit_button.columnconfigure(0, weight=1)
 
-screen = Tk()
-screen.geometry("300x300")
-main_screen(screen)
-screen.mainloop()
+# class main_screen:
+#     def __init__(self, master):
+#         self.master = master
+#         master.title("PACEMAKER 1.0")
+
+#         label = Label(master, text="Welcome to the PACEMAKER 1.0")
+#         label.grid(row=0, column=0)
+
+#         self.login_button = Button(master, text="Login")
+#         self.login_button.grid(row=1, column=0)
+
+#         self.register_button = Button(master, text="Register")
+#         self.register_button.grid(row=2, column=0)
+
+#         self.exit_button = Button(master, text="Exit", command=master.quit)
+#         self.exit_button.grid(row=3, column=0)
+
+#         # if checkComm() == True:
+#         Label(text = "Device is IS communicating with the DCM").grid(row=4, column=0)
+#         Label(text ="o",fg = "green").grid(row=4, column=1)
+#         # elif checkComm() == False:
+#         Label(text = "Device is NOT communicating with the DCM").grid(row=5, column=0)
+#         Label(text ="o",fg = "red").grid(row=5, column=1)
+
+#         master.columnconfigure(0, weight=1)
+#         master.rowconfigure(0, weight=1)
+#         self.login_button.columnconfigure(0, weight=1)
+#         self.register_button.columnconfigure(0, weight=1)
+#         self.exit_button.columnconfigure(0, weight=1)
+
+# screen = Tk()
+# screen.geometry("300x300")
+# main_screen(screen)
+# screen.mainloop()
