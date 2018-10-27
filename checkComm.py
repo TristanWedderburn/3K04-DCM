18ef4733ef122f2ab4245cec8f20efc733e249d7
Tristan Wedderburn <tristan.wedderburn8@gmail.com>
2018-10-27 17:22:50 -0400

    restructure for 3k04 assignment 1

--- checkComm.py
+++
@@ -1,8 +0,0 @@
-from Tkinter import *
-
-def checkComm():
-
-    # this function will check when the dcm and python are communicating, At the moment we are not required to serially communicate therefore we will return false
-
-    return False
-        
